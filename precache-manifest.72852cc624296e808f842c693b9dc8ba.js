8461c5c72225a9dcfa2faa06f4b25ff9e8230ff9
Alexander Fomin <firstmixlucker@gmail.com>
2020-05-22 23:05:17 +0300

    Updates

---
+++ precache-manifest.72852cc624296e808f842c693b9dc8ba.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e5a64eaccd4b39374765ada3a8469bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f62c9be97925a3a0bcf3",
+    "url": "/static/css/2.4c6ee7a4.chunk.css"
+  },
+  {
+    "revision": "cc1ae08befea600c87a9",
+    "url": "/static/css/main.671eb08c.chunk.css"
+  },
+  {
+    "revision": "f62c9be97925a3a0bcf3",
+    "url": "/static/js/2.c6a759e1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.c6a759e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc1ae08befea600c87a9",
+    "url": "/static/js/main.66c4455b.chunk.js"
+  },
+  {
+    "revision": "a591a506f741c0b25d82",
+    "url": "/static/js/runtime-main.8f1702d1.js"
+  },
+  {
+    "revision": "ce2f4444e50b985e600775eaed241e78",
+    "url": "/static/media/arrow.ce2f4444.svg"
+  },
+  {
+    "revision": "55730a14f1faf036b190dfb3263b9e25",
+    "url": "/static/media/dollar.55730a14.svg"
+  }
+]);
